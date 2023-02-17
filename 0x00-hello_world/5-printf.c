6f3fd14a7d280893beb0c6f697be0504c066d5d4
jesymaxil <jesymaxil97@gmail.com>
2023-02-16 17:23:21 -0800

    tired

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/** main - this is the success function
+ *
+ *  return: 0 when it works
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+} 
