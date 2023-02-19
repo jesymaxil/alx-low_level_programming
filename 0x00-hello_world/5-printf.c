3ed1a73f374d606a6936245783c2baa24a613648
jesymaxil <jesymaxil97@gmail.com>
2023-02-19 14:49:06 -0800

    baby

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-/* main-this is a success function */
-/* Description-this is a success function */
+/* main - this is a main function */
+/* Description - this is a success function */
  /* return: 0 when it works */
 
 
