3701f2a306f9a0f00a708990d5736cbb0922b7d0
jesymaxil <jesymaxil97@gmail.com>
2023-02-19 13:18:12 -0800

    damn

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/** main - this is the success function
+/** main:this is the success function
  *
  *  return: 0 when it works
  */
@@ -8,5 +8,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-	return(0);
-} 
+	return (0);
+}
